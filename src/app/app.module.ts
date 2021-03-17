b7e3bdf5373fd6fe254345c4a005436988612974
Ladislav Sokol <sokollac10@gmail.com>
2021-03-17 17:18:41 +0100

    login auth created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Emitters } from '../emitters/emitters';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+  authenticated = false;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    Emitters.authEmitter.subscribe((auth: boolean) => {
+      this.authenticated = auth;
+    });
+  }
+
+  logout(): void {
+    this.http.post('http://localhost:8000/api/logout', {}, {withCredentials: true})
+    .subscribe(() => { this.authenticated = false });
+  }
+}
