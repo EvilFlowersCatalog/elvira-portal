a0ad5d7344fd2e3dde9ec12fe2ae681c742a5804
Ladislav Sokol <sokollac10@gmail.com>
2021-08-17 14:04:42 +0200

    Started validation for title

---
+++ src/app/admin/validators/title.validator.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
+import { map } from 'rxjs/operators';
+import { AdminService } from '../services/admin.service';
+
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TitleValidators {
+  constructor(
+    private http: HttpClient,
+    private readonly adminService: AdminService,
+    ) {}
+
+  searchTitle(text) {
+          // Check if username is available
+      return this.adminService.checkTitle(text).pipe(
+        map(
+          data => {return data.items;}
+        )
+      );
+  }
+
+  userValidator(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return this.searchTitle(control.value).pipe(
+        map(
+          data => data.map(
+            resp => {
+              //console.log("control: ",control.value);
+              //console.log(resp.title);
+              if(resp.title.toLocaleLowerCase() === control.value.toLocaleLowerCase()){
+                console.log("found");
+                return { 'titleExists': true};
+              }
+
+              })
+        )
+        )
+    };
+  }
+
+}
+
+
