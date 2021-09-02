06398f7ea1a681dc43ef1c6fe28b2a068769a211
Michal <michal.pidanic1@gmail.com>
2021-09-02 13:32:11 +0200

    added filters prop to appState, created listener for filters change in home component, added entries for filtering, created feed tree in sidebar, created author autocomplete filter - not working yet, styled sidebar

--- src/app/library/services/filters/filters.service.ts
+++ src/app/library/services/filters/filters.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppStateService } from 'src/app/common/services/app-state/app-state.service';
+import { Authors, FeedTreeNode } from '../../library.types';
 import { ListEntriesResponse } from '../entries/entries.types';
 
 @Injectable({
@@ -19,16 +20,30 @@ export class FiltersService {
     });
   }
 
-  getFeedsTree() {
-    // return this.httpClient.get()
-  }
-
   entriesSearch(keyword: string): Observable<ListEntriesResponse> {
     let header = this.createAuthorizationHeader();
     let params = new HttpParams().set('title', keyword);
-    console.log(params);
+
     return this.httpClient.get<ListEntriesResponse>('api/apigw/entries', {
       params: params,
+      headers: header,
+    });
+  }
+
+  getAuthorSuggestions(query: string): Observable<Authors> {
+    let header = this.createAuthorizationHeader();
+    let params = new HttpParams().set('query', query);
+
+    return this.httpClient.get<Authors>(`api/apigw/authors`, {
+      params: params,
+      headers: header,
+    });
+  }
+
+  getFeedTreeNode(): Observable<FeedTreeNode> {
+    let header = this.createAuthorizationHeader();
+
+    return this.httpClient.get<FeedTreeNode>(`api/apigw/feed-tree`, {
       headers: header,
     });
   }
