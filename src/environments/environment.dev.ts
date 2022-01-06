6436be97ab1ee02f822136fe988ebc224a294ad8
Pavol Krajkovič <xkrajkovic@stuba.sk>
2022-01-06 18:52:41 +0000

    Update src/environments/environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: `http://gitlab.fiit.stuba.sk/api`,
+  baseUrl: `http://gitlab.fiit.stuba.sk`,
   assetsUrl: `http://gitlab.fiit.stuba.sk/assets`,
 };
