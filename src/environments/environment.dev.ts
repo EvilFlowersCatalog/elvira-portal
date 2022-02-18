5b008f4fec1558f9a0c795785d5653c116d6a5f6
Michal <michal.pidanic1@gmail.com>
2022-02-18 16:49:32 +0100

    changed dev env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: `http://elvira.workzone.fiit.stuba.sk`,
+  baseUrl: `http://api.elvira.workzone.fiit.stuba.sk`,
   assetsUrl: `https://elvira.workzone.fiit.stuba.sk/assets`,
 };
