4f08cfa898e08a2ba7e8ab0a6cab9d4831324973
Jakub Dubec <jakub.dubec@gmail.com>
2023-08-23 11:46:21 +0200

    Fixed catalogId

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: `https://evilflowers-dev.fiit.stuba.sk`,
   assetsUrl: `https://elvira-dev.fiit.stuba.sk/assets`,
-  catalogId: `b349b5a1-4d50-4bed-bf8d-c56d6c7b1280`
+  catalog_id: `b349b5a1-4d50-4bed-bf8d-c56d6c7b1280`
 };
