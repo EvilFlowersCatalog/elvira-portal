1712ced2230628f06ec4a4d0919b4350471fc0a4
Jakub Dubec <jakub.dubec@gmail.com>
2023-09-16 10:33:29 +0200

    Environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: `https://elvira.fiit.stuba.sk`,
+  baseUrl: `https://evilflowers.fiit.stuba.sk`,
   assetsUrl: `https://elvira.fiit.stuba.sk/assets`,
+  catalog_id: `b349b5a1-4d50-4bed-bf8d-c56d6c7b1280`
 };
