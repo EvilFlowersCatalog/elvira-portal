0ad0a4b3fa315a3b3b70465bed6a0d1b3c8d82c7
Tomáš Kordoš <kordo.t12@gmail.com>
2026-02-03 14:31:00 +0100

    fix books display

--- src/components/items/entry/display/EntriesWrapper.tsx
+++ src/components/items/entry/display/EntriesWrapper.tsx
@@ -4,7 +4,7 @@ export default function EntriesWrapper({ children, limitRows = false, className 
 return (
     <div
       className={twMerge(
-        `grid w-full grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-8 px-8 justify-items-center`,
+        `grid w-full grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-8 px-8 justify-items-center`,
         limitRows
           ? `
             lg:max-h-[calc(17rem*1)]
